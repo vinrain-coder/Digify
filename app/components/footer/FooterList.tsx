d31dc9fcf6ee70457457a78b632553c3abb95b21
vinrain-coder <143264723+vinrain-coder@users.noreply.github.com>
2024-09-19 15:17:54 +0300

    Update FooterList.tsx

--- app/components/footer/FooterList.tsx
+++ app/components/footer/FooterList.tsx
@@ -1,13 +1,13 @@
 interface FooterListProps {
-    children: React.ReactNode}
+  children: React.ReactNode;
+}
 
-const FooterList: React.FC<FooterListProps> = ({children}) => {
+const FooterList: React.FC<FooterListProps> = ({ children }) => {
   return (
-    <div className="w-full sm:w-0.5 md:w-0.25 lg:w-1/6 mb-6 flex flex-col gap-2">
-        {children}
+    <div className="w-full sm:w-1/2 md:w-1/4 lg:w-1/6 mb-8 flex flex-col gap-3">
+      {children}
     </div>
-  
-);
+  );
 };
 
 export default FooterList;
