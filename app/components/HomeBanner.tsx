b830e48a438684e20dd732794baad70c97e5fe8a
vinrain-coder <143264723+vinrain-coder@users.noreply.github.com>
2024-09-19 15:52:56 +0300

    Update HomeBanner.tsx

--- app/components/HomeBanner.tsx
+++ app/components/HomeBanner.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 const HomeBanner = () => {
   return (
-    <div className="relative bg-gradient-to-r from-sky-500 to-sky-700 h-screen flex items-center justify-center overflow-hidden">
-      <div className="relative z-10 flex flex-col md:flex-row items-center justify-center md:justify-between px-4 md:px-8 py-4 md:py-8">
+    <div className="relative bg-gradient-to-r from-sky-500 to-sky-700 mb-8 rounded-lg shadow-lg overflow-hidden">
+      <div className="mx-auto px-4 md:px-8 py-12 flex flex-col-reverse md:flex-row items-center justify-between">
         <div className="text-center md:text-left md:mr-8 max-w-lg">
           <h1 className="text-3xl md:text-4xl lg:text-5xl font-extrabold text-white leading-tight mb-4">
             Step Into Fashion Today
@@ -16,17 +16,17 @@ const HomeBanner = () => {
             GET 50% OFF ON YOUR FIRST ORDER!
           </p>
           <Link href="/shop">
-            <button className="bg-yellow-500 hover:bg-yellow-600 text-white font-semibold py-3 px-6 rounded-full text-lg md:text-xl transition-transform transform hover:scale-105">
+            <button className="bg-yellow-500 hover:bg-yellow-600 text-white font-semibold py-3 px-6 rounded-full text-lg md:text-xl transition-transform transform hover:scale-105 animate-bounce hover:animate-none">
               Shop Now
             </button>
           </Link>
         </div>
 
-        <div className="hidden md:block relative w-full md:w-1/2 lg:w-2/5 h-full">
+        <div className="w-full md:w-1/2 lg:w-2/5 relative h-56 md:h-80 lg:h-[30rem]">
           <Image
             src="/banner-image.png"
             layout="fill"
-            objectFit="cover"
+            objectFit="contain"
             alt="Banner Image"
           />
         </div>
