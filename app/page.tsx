aa2791ddb0f554c4e730d8e22a602f06dddf1692
vinrain-coder <143264723+vinrain-coder@users.noreply.github.com>
2024-09-21 02:21:28 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import ProductCard from "./components/products/ProductCard";
 
 export default function Home() {
   return (
-    <div className="p-8">
+    <div className="p-2">
       <Container>
         <div>
           <HomeBanner />
