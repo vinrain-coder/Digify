2e662666347ac38198877bd9237db5fdb03cb748
vinrain-coder <143264723+vinrain-coder@users.noreply.github.com>
2024-09-21 02:28:45 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <div>
           <HomeBanner />
         </div>
-        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4">
           {products.map((product: any) => {
             return <ProductCard key={product.id} data={product} />;
           })}
