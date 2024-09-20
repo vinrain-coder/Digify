7a704ed8972024d43cbeafa86cf208af1c9ef091
vinrain-coder <143264723+vinrain-coder@users.noreply.github.com>
2024-09-20 15:48:03 +0000

    fix homepage bug

--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,15 @@ export default function Home() {
     <div className="p-8">
       <Container>
         <div>
-          <HomeBanner/>
+          <HomeBanner />
         </div>
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          {products.map((product:any)=>{
-            return <ProductCard data={product}/>
+          {products.map((product: any) => {
+            return <ProductCard key={product.id} data={product} />;
           })}
         </div>
       </Container>
     </div>
-  )
+  );
 }
+
