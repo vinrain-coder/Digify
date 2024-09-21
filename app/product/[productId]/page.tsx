2daa0424ce5542de429ff7df8d6a068bfdb7c5ee
vinrain-coder <143264723+vinrain-coder@users.noreply.github.com>
2024-09-21 10:53:17 +0300

    Update page.tsx

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -12,7 +12,7 @@ const Product = ({ params }: { params: IParams }) => {
   console.log("params", params);
 
   return (
-    <div className="p-2 mt-6">
+    <div className="p-2 mt-8">
       <Container>
         <ProductDetails product={product} />
         <div className="flex flex-col mt-20 gap-4">
