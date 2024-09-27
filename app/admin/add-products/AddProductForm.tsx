7036c0a767c6996990b3f922890ab746a379c049
vinrain <143264723+vinrain-coder@users.noreply.github.com>
2024-09-27 10:24:33 +0000

    add product route

---
+++ app/admin/add-products/AddProductForm.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const AddProductForm = () => {
+  return <div>add p</div>;
+};
+
+export default AddProductForm;
---
+++ libs/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBARs0FMe_TjuYLlE_GR2FDjg_RWsGz8Jw",
+  authDomain: "shoepedi-da39e.firebaseapp.com",
+  projectId: "shoepedi-da39e",
+  storageBucket: "shoepedi-da39e.appspot.com",
+  messagingSenderId: "17619938445",
+  appId: "1:17619938445:web:ffbb96bf7e04eca3d646c4",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
