9eb9601402ed087581dfc72c03620e717b3238f1
vinrain <143264723+vinrain-coder@users.noreply.github.com>
2024-09-30 12:16:53 +0000

    manage products

--- app/admin/add-products/page.tsx
+++ app/admin/add-products/page.tsx
@@ -12,7 +12,7 @@ const AddProducts = async () => {
   }
 
   return (
-    <div className="p-8">
+    <div>
       <Container>
         <FormWrap>
           <AddProductForm />
--- app/admin/manage-products/page.tsx
+++ app/admin/manage-products/page.tsx
@@ -1,5 +1,26 @@
-const ManageProducts = () => {
-  return <div>manage products</div>;
+import Container from "@/app/components/Container";
+import ManageProductsClient from "./ManageProductsClient";
+import getProducts from "@/actions/getProducts";
+import { getCurrentUser } from "@/actions/getCurrentUser";
+import NullData from "@/app/components/NullData";
+
+const ManageProducts = async () => {
+  const products = await getProducts({
+    category: null,
+  });
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser || currentUser.role !== "ADMIN") {
+    return <NullData title="OOps! Access denied" />;
+  }
+
+  return (
+    <div>
+      <Container>
+        <ManageProductsClient products={products} />
+      </Container>
+    </div>
+  );
 };
 
 export default ManageProducts;
