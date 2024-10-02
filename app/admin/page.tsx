b70eeeeaa92aa376f6650386b8a892d4aa2b26b1
vinrain-coder <vincentombogo57@gmail.com>
2024-10-02 10:43:44 +0000

    completed summary

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,20 +1,20 @@
-import getProducts from "@/actions/getProducts";
-import Summary from "./Summary";
-import getOrders from "@/actions/getOrders";
-import getUsers from "@/actions/getUsers";
-import Container from "../components/Container";
+// import getProducts from "@/actions/getProducts";
+// import Summary from "./Summary";
+// import getOrders from "@/actions/getOrders";
+// import getUsers from "@/actions/getUsers";
+// import Container from "../components/Container";
 
-const Admin = async () => {
-  const products = await getProducts({ category: null });
-  const orders = await getOrders();
-  const users = await getUsers();
-  return (
-    <div className=" p-8">
-      <Container>
-        <Summary products={products} orders={orders} users={users} />
-      </Container>
-    </div>
-  );
-};
+// const Admin = async () => {
+//   const products = await getProducts({ category: null });
+//   const orders = await getOrders();
+//   const users = await getUsers();
+//   return (
+//     <div className=" p-8">
+//       <Container>
+//         <Summary products={products} orders={orders} users={users} />
+//       </Container>
+//     </div>
+//   );
+// };
 
-export default Admin;
+// export default Admin;
