a72f4cb5073b4f89a20ce29191374ff3095956b8
vinrain-coder <vincentombogo57@gmail.com>
2024-10-08 00:06:32 +0000

    added order schema and payment intent route

---
+++ app/api/create-payment-intent/route.ts
@@ -0,0 +1,53 @@
+import Stripe from "stripe";
+import prisma from "@/libs/prismadb";
+import { NextResponse } from "next/server";
+import { CartProductType } from "@/app/product/[productId]/ProductDetails";
+import { getCurrentUser } from "@/actions/getCurrentUser";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: "2022-11-15",
+});
+
+const calculateOrderAmount = (items: CartProductType[]) => {
+  const totalPrice = items.reduce((acc, item) => {
+    const itemTotal = item.price * item.quantity;
+
+    return acc + itemTotal;
+  }, 0);
+
+  return totalPrice;
+};
+
+export async function POST(request: Request) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const body=await request.json()
+  const {items, payment_intent_id}=body
+  const total=calculateOrderAmount(items)*100
+  const orderData={
+    user:{connect: {id:currentUser.id}},
+    amount:total,
+    currency:'usd',
+    status:"pending",
+    deliveryStatus:"pending",
+    paymentIntentId: payment_intent_id,
+    products:items
+  }
+
+  if(!payment_intent_id){
+    //update order
+  } else{
+    //create payment intent
+    const payment_intent_id=await stripe.paymentIntents.create({
+      amount:total,
+      currency:'usd'
+      automatic_payment_methods: {enabled:true}
+    })
+    //ceate the order
+    
+  }
+}
